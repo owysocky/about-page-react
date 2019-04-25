1544c94b82d46a8d28ee653fc21401b2a8b249a7
Olha Wy and Yulia Sh <yulia@>
2019-04-25 14:04:24 -0700

    style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,25 +8,44 @@ function Header(){
     color: '#75c603'
   }
   return(
-    <div>
+    <div className="main">
       <style jsx>{`
         p{
           display: inline-block;
           padding: 20px;
           padding-top: 30px;
         }
-        div{
-          text-align: center;
-        }
         img{
           width: 100px;
           float: left;
         }
+        .main{
+          text-align: center;
+          background-color: white;
+          height:100px;
+          width: 100%;
+          background-image: none;
+          background-repeat: repeat;
+          background-attachment: scroll;
+          background-position: 0% 0%;
+          position: fixed;
+          top: 0pt;
+          left: 0pt;
+
+          border-bottom: 4px solid white;
+
+        }
+        .border-line{
+          margin-bottom: 2px;
+          border-bottom: 2px solid #75c603;
+        }
       `}</style>
+    <div className="border-line">
       <img src={logo}></img>
       <p><Link to="/" style={linkDecoration}>Home</Link></p>
       <p><Link to="/team" style={linkDecoration}>Our Team</Link></p>
       <p><Link style={linkDecoration} to="">Locations</Link></p>
+    </div>
     </div>
   );
 }
