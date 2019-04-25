9b89f6196a02e1bbf666b00c4dd11a507664400a
Olha Wy and Yulia Sh <yulia@>
2019-04-25 14:12:43 -0700

    add grid

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -20,15 +20,16 @@ function Team(){
         }
         .container {
           display: grid;
-          grid-template-columns: 1fr 1fr;
+          grid-template-columns: 1fr 1fr 1fr;
         }
       `}</style>
     <h3>That's our team!</h3>
 
-      <div className="containter">
+      <div className="container">
         <div>
           <img src={julia}></img>
         </div>
+        <div></div>
         <div>
           <h3>Julia</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -36,6 +37,7 @@ function Team(){
         <div>
           <img src={olha}></img>
         </div>
+        <div></div>
         <div>
           <h3>Olya</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
@@ -43,6 +45,7 @@ function Team(){
         <div>
           <img src={biker}></img>
         </div>
+        <div></div>
         <div>
           <h3>Lorem</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
@@ -50,6 +53,7 @@ function Team(){
         <div>
           <img src={biker1}></img>
         </div>
+        <div></div>
         <div>
           <h3>Ipsum</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
