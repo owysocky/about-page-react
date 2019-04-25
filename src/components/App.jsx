3adddee05da750de198f0d8a0bc8501185023749
Olha Wy and Yulia Sh <olha@>
2019-04-25 09:34:25 -0700

    initial set up

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route } from 'react-router-dom';
+
+
+
+function App(){
+  return (
+    <div>
+      <Switch>
+        <h1>Works!</h1>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
